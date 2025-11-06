66954d0a4d47172e56279f098550385f497c1964
floxo115 <flojak@gmx.net>
2025-11-06 11:49:42 +0100

    removed files

--- fusinter_python_implementation/__init__.py
+++
@@ -1,1 +0,0 @@
-from .fusinter_v2 import FUSINTERDiscretizer
--- run_tests.sh
+++
@@ -1,8 +0,0 @@
-echo "running hctl evaluation on artificial data"
-python train_and_evaluate_hclt_models.py
-
-echo "running own model evaluation on artificial data"
-python train_and_evaluate_own_models.py
-
-echo "running own model on hepatitis data"
-python train_and_evaluate_hepatitis_ds_multiple_cats.py > results_for_hepatitis.txt
